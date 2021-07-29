893c773549c65a6e1d06bb583db25ff72d589e5d
keidrumfreak <kei.drumfreak@gmail.com>
2021-07-29 19:54:47 +0900

    TableData部分を部品化

---
+++ BmsManager/ViewModel/BmsTableDataListViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BmsManager.Data;
+using CommonLib.Wpf;
+using Microsoft.EntityFrameworkCore;
+
+namespace BmsManager
+{
+    class BmsTableDataListViewModel : ViewModelBase
+    {
+        public bool Narrowed { get; set; }
+
+        public ICommand ChangeNarrowing { get; set; }
+
+        BmsTableViewModel table;
+        public BmsTableViewModel Table
+        {
+            get { return table; }
+            set { SetProperty(ref table, value); loadTableData(); }
+        }
+
+        IEnumerable<BmsTableData> tableDatas;
+        public IEnumerable<BmsTableData> TableDatas
+        {
+            get { return tableDatas; }
+            set { SetProperty(ref tableDatas, value); }
+        }
+
+        public BmsTableDataListViewModel()
+        {
+            ChangeNarrowing = CreateCommand(input => loadTableData());
+        }
+
+        private void loadTableData()
+        {
+            TableDatas =Table?.TableDatas.ToArray();
+
+            if (Table == null)
+                return;
+
+            if (Narrowed)
+            {
+                using (var con = new BmsManagerContext())
+                {
+                    var query = con.Files.Join(con.TableDatas,
+                        f => f.MD5, d => d.MD5, (f, d) => d);
+                    if (Table.IsTable)
+                    {
+                        query = query.Where(d => d.Difficulty.BmsTableID == Table.ID);
+                    }
+                    else
+                    {
+                        query = query.Where(d => d.BmsTableDifficultyID == Table.ID);
+                    }
+                    var existFile = query.AsNoTracking().ToArray();
+                    TableDatas = TableDatas.Where(d => !existFile.Any(f => f.MD5 == d.MD5)).ToArray();
+                }
+            }
+        }
+    }
+}
--- BmsManager/ViewModel/BmsTableManagerViewModel.cs
+++ BmsManager/ViewModel/BmsTableManagerViewModel.cs
@@ -15,23 +15,13 @@ namespace BmsManager
 {
     class BmsTableManagerViewModel : ViewModelBase
     {
-        public bool Narrowed { get; set; }
-
-        public ICommand ChangeNarrowing { get; set; }
-
-        IEnumerable<BmsTableData> tableDatas;
-        public IEnumerable<BmsTableData> TableDatas
-        {
-            get { return tableDatas; }
-            set { SetProperty(ref tableDatas, value); }
-        }
-
         public BmsTableTreeViewModel BmsTableTree { get; set; }
+        public BmsTableDataListViewModel TableDataList { get; set; }
 
         public BmsTableManagerViewModel()
         {
             BmsTableTree = new BmsTableTreeViewModel();
-            ChangeNarrowing = CreateCommand(loadTableData);
+            TableDataList = new BmsTableDataListViewModel();
             BmsTableTree.PropertyChanged += BmsTableTree_PropertyChanged;
         }
 
@@ -39,31 +29,7 @@ namespace BmsManager
         {
             if (e.PropertyName == nameof(BmsTableTreeViewModel.SelectedTreeItem))
             {
-                loadTableData(null);
-            }
-        }
-
-        private void loadTableData(object input)
-        {
-            TableDatas = BmsTableTree.SelectedTreeItem?.TableDatas.ToArray();
-
-            if (Narrowed)
-            {
-                using (var con = new BmsManagerContext())
-                {
-                    var query = con.Files.Join(con.TableDatas,
-                        f => f.MD5, d => d.MD5, (f, d) => d);
-                    if (BmsTableTree.SelectedTreeItem.IsTable)
-                    {
-                        query = query.Where(d => d.Difficulty.BmsTableID == BmsTableTree.SelectedTreeItem.ID);
-                    }
-                    else
-                    {
-                        query = query.Where(d => d.BmsTableDifficultyID == BmsTableTree.SelectedTreeItem.ID);
-                    }
-                    var existFile = query.AsNoTracking().ToArray();
-                    TableDatas = TableDatas.Where(d => !existFile.Any(f => f.MD5 == d.MD5)).ToArray();
-                }
+                TableDataList.Table = BmsTableTree.SelectedTreeItem;
             }
         }
     }
