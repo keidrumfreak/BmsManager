37cb4fcc80553ff575b9bb57bdfbba4ccb16f95c
keidrumfreak <kei.drumfreak@gmail.com>
2021-08-09 02:50:28 +0900

    差分一括DL機能拡張

--- BmsManager/ViewModel/BmsTableDataViewModel.cs
+++ BmsManager/ViewModel/BmsTableDataViewModel.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using BmsManager.Data;
 using CommonLib.IO;
@@ -31,6 +32,8 @@ namespace BmsManager
 
         public ICommand OpenPackUrl { get; }
 
+        public ICommand OpenLR2IR { get; }
+
         BmsTableData data;
 
         public BmsTableDataViewModel(BmsTableData data)
@@ -40,6 +43,7 @@ namespace BmsManager
             OpenUrl = CreateCommand(input => openUrl(data.Url), input => !string.IsNullOrEmpty(data.Url));
             OpenDiffUrl = CreateCommand(input => openUrl(data.DiffUrl), input => !string.IsNullOrEmpty(data.DiffUrl));
             OpenPackUrl = CreateCommand(input => openUrl(data.PackUrl), input => !string.IsNullOrEmpty(data.PackUrl));
+            OpenLR2IR = CreateCommand(input => openUrl($"http://www.dream-pro.info/~lavalse/LR2IR/search.cgi?mode=ranking&bmsmd5={data.MD5}"), input => !string.IsNullOrEmpty(data.MD5));
         }
 
         private void openUrl(string url)
@@ -49,29 +53,64 @@ namespace BmsManager
 
         public async Task DownloadAsync(string targetDir)
         {
+            
+            if (!string.IsNullOrEmpty(data.DiffUrl))
+            {
+                var targetPath = getTargetPath(targetDir, data.DiffUrl);
+                if (!string.IsNullOrEmpty(targetPath))
+                {
+                    try
+                    {
+                        var client = HttpClientProvider.GetClient();
+                        using (var res = await client.GetAsync(data.DiffUrl))
+                        using (var stream = await res.Content.ReadAsStreamAsync())
+                        using (var fs = SystemProvider.FileSystem.FileStream.Create(targetPath, FileMode.Create, FileAccess.Write, FileShare.None))
+                        {
+                            stream.CopyTo(fs);
+                        }
+                        return;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"{data.DiffUrl}\r\n{ex}");
+                    }
+                }
+            }
+            if (!string.IsNullOrEmpty(data.PackUrl))
+            {
+                var targetPath = getTargetPath(targetDir, data.PackUrl);
+                try
+                {
+                    if (!string.IsNullOrEmpty(targetPath))
+                    {
+                        var client = HttpClientProvider.GetClient();
+                        using (var res = await client.GetAsync(data.PackUrl))
+                        using (var stream = await res.Content.ReadAsStreamAsync())
+                        using (var fs = SystemProvider.FileSystem.FileStream.Create(targetPath, FileMode.Create, FileAccess.Write, FileShare.None))
+                        {
+                            stream.CopyTo(fs);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{data.PackUrl}\r\n{ex}");
+                }
+            }
+        }
+
+        private string getTargetPath(string targetDir, string url)
+        {
             var exts = BmsExtension.GetExtensions();
-            if (!string.IsNullOrEmpty(data.DiffUrl) && (exts.Any(e => data.DiffUrl.EndsWith(e)) || data.DiffUrl.EndsWith("zip")))
+            if (exts.Any(e => data.DiffUrl.EndsWith(e)) || data.DiffUrl.EndsWith("zip"))
             {
-                var targetPath = PathUtil.Combine(targetDir, Path.GetFileName(data.DiffUrl));
-                var client = HttpClientProvider.GetClient();
-                using (var res = await client.GetAsync(data.DiffUrl))
-                using (var stream = await res.Content.ReadAsStreamAsync())
-                using (var fs = SystemProvider.FileSystem.FileStream.Create(targetPath, FileMode.Create, FileAccess.Write, FileShare.None))
-                {
-                    stream.CopyTo(fs);
-                }
+                return PathUtil.Combine(targetDir, Path.GetFileName(data.DiffUrl));
             }
-            else if (!string.IsNullOrEmpty(data.PackUrl) && (exts.Any(e => data.PackUrl.EndsWith(e)) || data.PackUrl.EndsWith("zip")))
+            else if (data.DiffUrl.Contains("get="))
             {
-                var targetPath = PathUtil.Combine(targetDir, Path.GetFileName(data.PackUrl));
-                var client = HttpClientProvider.GetClient();
-                using (var res = await client.GetAsync(data.PackUrl))
-                using (var stream = await res.Content.ReadAsStreamAsync())
-                using (var fs = SystemProvider.FileSystem.FileStream.Create(targetPath, FileMode.Create, FileAccess.Write, FileShare.None))
-                {
-                    stream.CopyTo(fs);
-                }
+                return PathUtil.Combine(targetDir, data.DiffUrl.Substring(data.DiffUrl.IndexOf("get=")).Replace("get=", "") + ".zip");
             }
+            return null;
         }
     }
 }
